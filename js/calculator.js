76d8716a4875c11c3ab117fdbfee5b2cd1c6d3b6
siaoliu66 <63837974+siaoliu66@users.noreply.github.com>
2021-10-18 11:56:08 +0800

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -2,7 +2,7 @@ function cal() {
     var price = parseInt($(".price").val())
     var Shipping = parseInt($(".shipping").val())
     var num = parseFloat($(".num").val())
-    var totalPrice = Math.round((price + Shipping) * 271 / 1000)
+    var totalPrice = Math.round((price + Shipping) * 270 / 1000)
     var finalPrice = Math.round(totalPrice + num * 250 + 60)
     // var vipPrice
 //   var text = `<p>📣報價如下</br>🔷商品金額：`
@@ -11,33 +11,33 @@ function cal() {
 //             vipPrice = totalPrice - 60
 //             finalPrice = Math.round(vipPrice + num * 250 + 60)
 //             if (Shipping == 0) {
-//                 text += `<span class="Price">${toCurrency(price)}</span> * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 60 = ${toCurrency(vipPrice)}`
+//                 text += `<span class="Price">${toCurrency(price)}</span> * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 60 = ${toCurrency(vipPrice)}`
 //             } else {
-//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 60 = ${toCurrency(vipPrice)}`
+//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 60 = ${toCurrency(vipPrice)}`
 //             }
 //         } else if (totalPrice >= 6000 && totalPrice < 8999) {
 //             vipPrice = totalPrice - 100
 //             finalPrice = Math.round(vipPrice + num * 250 + 60)
 //             if (Shipping == 0) {
-//                 text += `<span class="Price">${toCurrency(price)}</span>* 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 100 = ${toCurrency(vipPrice)}`
+//                 text += `<span class="Price">${toCurrency(price)}</span>* 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 100 = ${toCurrency(vipPrice)}`
 //             } else {
-//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 100 = ${toCurrency(vipPrice)}`
+//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 100 = ${toCurrency(vipPrice)}`
 //             }
 //         } else if (totalPrice >= 9000 && totalPrice < 11999) {
 //             vipPrice = totalPrice - 150
 //             finalPrice = Math.round(vipPrice + num * 250 + 60)
 //             if (Shipping == 0) {
-//                 text += `<span class="Price">${toCurrency(price)}</span>* 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 150 = ${toCurrency(vipPrice)}`
+//                 text += `<span class="Price">${toCurrency(price)}</span>* 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 150 = ${toCurrency(vipPrice)}`
 //             } else {
-//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 150 = ${toCurrency(vipPrice)}`
+//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 150 = ${toCurrency(vipPrice)}`
 //             }
 //         } else if (totalPrice >= 12000) {
 //             vipPrice = totalPrice - 200
 //             finalPrice = Math.round(vipPrice + num * 250 + 60)
 //             if (Shipping == 0) {
-//                 text += `<span class="Price">${toCurrency(price)}</span>* 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 200 = ${toCurrency(vipPrice)}`
+//                 text += `<span class="Price">${toCurrency(price)}</span>* 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 200 = ${toCurrency(vipPrice)}`
 //             } else {
-//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 200 = ${toCurrency(vipPrice)}`
+//                 text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)] * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>🔷週年慶滿額折抵：${toCurrency(totalPrice)} - 200 = ${toCurrency(vipPrice)}`
 //             }
 //         }else{
 //             alert('您尚未符合滿額折扣資格~\n>>商品金額約11,000，才會符合週年慶滿額折底呦！！')
@@ -49,7 +49,7 @@ function cal() {
 //         if (Shipping == 0) {
 //             text += `<span class="Price">${toCurrency(price)}</span>`
 //         } else { text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)]` }
-//         text += ` * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>
+//         text += ` * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>
 // 🔷國際運費：1公斤250元</br>
 // 🔷應付金額：<span class="totalPrice">${toCurrency(totalPrice)}</span> + <span class="Num">250*${num}</span>(重量預估${num}kg以內) + 60(超商運費) = <span class="finalPrice">${toCurrency(finalPrice)}</span></p>`
 //     }
@@ -57,7 +57,7 @@ var text = `<p>📣報價如下</br>🔷商品金額：`
 if (Shipping == 0) {
         text += `<span class="Price">${toCurrency(price)}</span>`
     } else { text += `[<span class="Price">${toCurrency(price)}</span> + <span class="Shipping">${toCurrency(Shipping)}</span>(日本境內運費)]` }
-    text += ` * 0.271 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>
+    text += ` * 0.270 = <span class="totalPrice">${toCurrency(totalPrice)}</span></br>
 🔷國際運費：1公斤250元</br>
 🔷應付金額：<span class="totalPrice">${toCurrency(totalPrice)}</span> + <span class="Num">250*${num}</span>(重量預估${num}kg以內) + 60(超商運費) = <span class="finalPrice">${toCurrency(finalPrice)}</span></p>`
 
